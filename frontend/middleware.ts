4aed58277e4926dffed87fa4a86474fe306367bb
yash <yashshembe14@gmail.com>
2026-03-10 21:22:03 +0530

    commented middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,22 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("accessToken")?.value;
-
-  const { pathname } = request.nextUrl;
-
-  const isDashboard =
-    pathname.startsWith("/dashboard") ||
-    pathname.startsWith("/dashboard-admin");
-
-  // If not logged in → block dashboard
-  if (!token && isDashboard) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
+  // TEMPORARY: disable middleware
   return NextResponse.next();
 }
 
+/*
 export const config = {
   matcher: ["/dashboard/:path*", "/dashboard-admin/:path*"],
 };
+*/
