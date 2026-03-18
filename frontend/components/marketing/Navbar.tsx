da79fd01a0dc88762e710595a7caf85b8e65c706
yash <yashshembe14@gmail.com>
2026-03-18 21:35:12 +0530

    chnage name

--- frontend/components/marketing/Navbar.tsx
+++ frontend/components/marketing/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar() {
         {/* Logo */}
         <div className="flex items-center gap-2">
           <img
-            src="/transparent.jpg" // ✅ correct path
+            src="/eye1.jpg" // ✅ correct path
             alt="GlucoScan Logo"
             className="h-8 w-8 object-contain"
           />
